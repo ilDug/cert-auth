c7a709d6f53fecd9f6ea02bb3910afcb42e2b283
ilDug <dognini.marco@gmail.com>
2022-10-18 12:09:25 +0200

    prima emissione

--- GUIDE.md
+++
@@ -1,7 +0,0 @@
-# Guida di riferimento
-
-segui questo [link](https://jamielinux.com/docs/openssl-certificate-authority/introduction.html) per la guida online
-
-
-
-
--- docker-cmd.bash
+++ docker-cmd.bash
@@ -1,3 +1,5 @@
-docker run -it --rm  --name myCA -v ${pwd}/app:/app -v ${pwd}/PKI:/PKI cert-auth
+docker run -it --rm  -v ${pwd}/app:/app -v ${pwd}/PKI:/PKI cert-auth
 
+docker run -it --rm -v ${pwd}/PKI:/PKI cert-auth
 
+docker build -t cert-auth .
